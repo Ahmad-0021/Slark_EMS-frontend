cee4e11ad5ca449da922c7bbf7afb557afb09212
Muhammad Ahmad <ah167493991@gmail.com>
2025-01-06 19:16:58 +0500

    some fixes

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -4,7 +4,8 @@ import axios from "axios";
 export const login = async (data: Ilogin) => {
   const response = await axios.post<IloginResponse>(
     `${process.env.NEXT_PUBLIC_API_URL}/auth/login`,
-    data
+    data,
+    { withCredentials: true, headers: { "Content-Type": "application/json" } }
   );
 
   return response.data;
